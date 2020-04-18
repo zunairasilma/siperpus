5536a8567bdf4f0f8e6408297e7dffff69895f23
zunairasilma <63899821+zunairasilma@users.noreply.github.com>
2020-04-18 21:56:57 +0700

    Create header.php

---
+++ aset/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta "charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<link rel="stylesheet" href="http://localhost/siperpus/aset/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://localhost/siperpus/aset/fontawesome/css/all.min.css">
+
+	<title>SiPERPUS</title>
+</head>
+<body >
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><i class="fas fa-book-reader text-white mr-2"></i>SiPERPUS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="http://localhost/siperpus/index.php">Dashboard</a>
+      <a class="nav-item nav-link" href="http://localhost/siperpus/buku/index.php">Buku</a>
+      <a class="nav-item nav-link" href="http://localhost/siperpus/anggota/index.php">Anggota</a>
+      <a class="nav-item nav-link" href="http://localhost/siperpus/transaksi/index.php">Peminjaman</a>
+      <a class="nav-item nav-link" href="#">Logout</a>
+    </div>
+  </div>
+</nav>
